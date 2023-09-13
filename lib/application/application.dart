658196ae8a22f8e2137e42a6ddff0fb0c7cdcc2e
akshay <akshayt@rgcdynamics.com>
2023-09-13 14:54:01 +0530

    after the register_controller pafe recode

---
+++ lib/application/application.dart
@@ -0,0 +1,10 @@
+// class ApplicationDartFile extends StatelessWidget {
+//   const ApplicationDartFile({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//
+//     );
+//   }
+// }
--- lib/pages/registeration/register_controller.dart
+++ lib/pages/registeration/register_controller.dart
@@ -1,55 +1,55 @@
 import 'package:eccommerce2/common/widgets/toasts.dart';
 import 'package:eccommerce2/pages/registeration/bloc/register_bloc.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class RegisterController{
   final BuildContext context;
   RegisterController({required this.context});
-Future<void> handleEmailRegiste()
-async {
-  final state=context.read<RegisterBloc>().state;
-  String userName=state.username;
-   String email=state.email;
-   String password=state.password;
-   String confirmaPAssword=state.confirmPassword;
-   if(userName.isEmpty){
-     toastInfo(msg: "Please enter your Name");
-     return;
-   }
-   if(email.isEmpty){
-     toastInfo(msg: "Please enter you Email");
-     return;
-   }if(password.isEmpty){
-     toastInfo(msg: "Please enter your Password");
-     return;
-   }if(confirmaPAssword.isEmpty){
-     toastInfo(msg: "Please enter Confirm Password");
-     return;
-   }
+   Future<void> handleEmailRegiste() async {
+     final state=context.read<RegisterBloc>().state;
+     String userName=state.username;
+     String email=state.email;
+     String password=state.password;
+     String cofirmPasswod=state.confirmPassword;
 
-   try{
-      final credential=await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password);
-      if(credential.user!=null){
-      await  credential.user?.sendEmailVerification();
-      await  credential.user?.updateDisplayName(userName);
-      toastInfo(msg: "an email send to your registered email.To activate please click the link");
-      Navigator.of(context).pop();
-      }
-
-   }on FirebaseAuthException  catch(e){
-     if(e.code=="week_password"){
-       toastInfo(msg: "Your password is week");
-
-     }else if(e.code=="email=already-in-use"){
-       toastInfo(msg: "The email is already in use");
-     }else if(e.code=="invalid-email"){
-       toastInfo(msg: "The email is invalid");
+     if(userName.isEmpty){
+       toastInfo(msg: "Please enter your username");
+       return;
+     }  if(email.isEmpty){
+       toastInfo(msg: "Please enter your email");
+       return;
+     }  if(password.isEmpty){
+       toastInfo(msg: "Please enter your password");
+       return;
+     }  if(cofirmPasswod.isEmpty){
+       toastInfo(msg: "Please enter your confirm password");
+       return;
      }
-   }
+     try{
+       final credential=await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password);
+       if(credential.user!=null){
+      await   credential.user?.sendEmailVerification();
+      await   credential.user?.updateDisplayName(userName);
+      Navigator.of(context).pop();
+       }
+     }on FirebaseAuthException catch(e){
+       if(e.code=="email-already-in-use"){
+         toastInfo(msg: "Your email already exist");
 
-}
+       }else  if(e.code=="invalid-email"){
+         toastInfo(msg: "Your email is invalid");
+
+       }else  if(e.code=="weak-password"){
+         toastInfo(msg: "Your password is week");
+
+       }
+     }
+
+
+   }
 
 
 }
